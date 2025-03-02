2c3bf2cf825b2130806420d1b163a100ff77ebbc
Francis <francis1592@gmail.com>
2025-03-02 13:00:57 -0500

    wip

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,8 +1,5 @@
 export const environment = {
   production: false,
-  api: {
-    serverUrl: 'http://127.0.0.1:8787'
-  },
   auth0: {
     domain: 'dev-7y1grk6neur7cepa.us.auth0.com',
     clientId: 'tVKlDFBcmF9XHDtcNlOZJAKNfNyYPqBa',
@@ -10,5 +7,8 @@ export const environment = {
       redirect_uri: 'http://localhost:4200/callback',
       audience: 'https://gayness-scale-backend/'
     }
+  },
+  api: {
+    serverUrl: 'http://127.0.0.1:8787'
   }
 }; 
